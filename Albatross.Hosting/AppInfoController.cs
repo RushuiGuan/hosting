79cbe972379e08f90ac3764303d9bb2624b14e86
rushuiguan <rushui@outlook.com>
2025-04-28 09:53:09 -0400

    code cleanup

--- Albatross.Hosting/AppInfoController.cs
+++ Albatross.Hosting/AppInfoController.cs
@@ -3,13 +3,10 @@ using Albatross.Config;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
-using System.Reflection;
-using System.Text;
 
 namespace Albatross.Hosting {
 	[Route("api/app-info")]
 	[ApiController]
-	[Authorize]
 	public class AppInfoController : ControllerBase {
 		private readonly ProgramSetting programSetting;
 		private readonly IGetCurrentUser getCurrentUser;
@@ -24,25 +21,14 @@ namespace Albatross.Hosting {
 		[HttpGet]
 		public ProgramSetting Get() => programSetting;
 
-		[HttpGet("versions")]
-		public string Versions() {
-			var sb = new StringBuilder();
-			var assembly = Assembly.GetEntryAssembly();
-			if (assembly != null) {
-				sb.AppendLine(assembly.FullName);
-				foreach (var refAssembly in assembly.GetReferencedAssemblies()) {
-					sb.AppendLine(refAssembly.FullName);
-				}
-			}
-			return sb.ToString();
-		}
-
 		[HttpGet("env")]
 		public EnvironmentSetting GetEnvironment() => environmentSetting;
 
+		[Authorize]
 		[HttpGet("user-claim")]
 		public string[] GetUserClaims() => HttpContext.User?.Claims?.Select(args => $"{args.Type}: {args.Value}")?.ToArray() ?? new string[0];
 
+		[Authorize]
 		[HttpGet("user")]
 		public string GetCurrentUser() => getCurrentUser.Get();
 	}
