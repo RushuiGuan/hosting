662c21f3a4ba46be232b2699281d78d23d045602
rushuiguan <rushui@gmail.com>
2026-03-23 09:04:19 -0400

    wip

--- Albatross.Hosting/Setup.cs
+++ Albatross.Hosting/Setup.cs
@@ -17,14 +17,22 @@ namespace Albatross.Hosting {
 		protected IConfiguration configuration;
 		string environment { get; init; }
 		protected SetupSerilog setupSerilog;
-
-		public Setup(string[] args, bool supressUnhandledArgumentExceptionLogging = false) {
+		string GetConfigRoot(string[] args, string? environmentalPrefix) {
+			var builder = new ConfigurationBuilder()
+				.AddEnvironmentVariables(environmentalPrefix)
+				.AddCommandLine(args);
+			var configRoot = builder.Build()["config-root"];
+			return configRoot ?? AppContext.BaseDirectory;
+		}
+		public Setup(string[] args, bool supressUnhandledArgumentExceptionLogging = false) : this(args, null, supressUnhandledArgumentExceptionLogging) { }
+		public Setup(string[] args, string? environmentPrefix, bool supressUnhandledArgumentExceptionLogging) {
 			this.SupressUnhandledArgumentExceptionLogging = supressUnhandledArgumentExceptionLogging;
 			environment = EnvironmentSetting.ASPNETCORE_ENVIRONMENT.Value;
 			hostBuilder = Host.CreateDefaultBuilder(args).UseSerilog();
-			this.setupSerilog = ConfigureLogging(new SetupSerilog(), environment, args);
+			var configRoot = GetConfigRoot(args, environmentPrefix);
+			this.setupSerilog = ConfigureLogging(new SetupSerilog(), configRoot, environment, args);
 			var configBuilder = new ConfigurationBuilder()
-				.SetBasePath(AppContext.BaseDirectory)
+				.SetBasePath(configRoot)
 				.AddJsonFile("appsettings.json", false, true);
 			if (!string.IsNullOrEmpty(environment)) { configBuilder.AddJsonFile($"appsettings.{environment}.json", true, true); }
 
@@ -43,17 +51,17 @@ namespace Albatross.Hosting {
 		/// If true, unhandled ArgumentException will be not be logged.  This is useful for public faceing API where the client often send bad request.
 		/// </summary>
 		public bool SupressUnhandledArgumentExceptionLogging { get; private set; }
+
 		private readonly static ScalarValue ExceptionHandlerMiddlewareSourceContext = new ScalarValue("Microsoft.AspNetCore.Diagnostics.ExceptionHandlerMiddleware");
-		protected virtual SetupSerilog ConfigureLogging(SetupSerilog setup, string environment, string[] args) {
+		protected virtual SetupSerilog ConfigureLogging(SetupSerilog setup, string environment, string configRoot, string[] args) {
 			if (SupressUnhandledArgumentExceptionLogging) {
 				setup.Configure(c => c.Filter.ByExcluding(e => e.Exception is ArgumentException
-					&& e.Properties.TryGetValue(Serilog.Core.Constants.SourceContextPropertyName, out var sourceContext)
-					&& sourceContext.Equals(ExceptionHandlerMiddlewareSourceContext)));
+				                                               && e.Properties.TryGetValue(Serilog.Core.Constants.SourceContextPropertyName, out var sourceContext)
+				                                               && sourceContext.Equals(ExceptionHandlerMiddlewareSourceContext)));
 			}
-			setup.UseConfigFile(environment, null, args);
+			setup.UseConfigFile(environment, configRoot, args);
 			return setup;
 		}
-
 		public virtual Setup RunAsService() {
 			var setting = new ProgramSetting(configuration);
 			switch (setting.ServiceManager) {
@@ -68,7 +76,6 @@ namespace Albatross.Hosting {
 			}
 			return this;
 		}
-
 		public virtual Setup ConfigureWebHost<Startup>() where Startup : Hosting.Startup {
 			hostBuilder.ConfigureWebHostDefaults(webBuilder => {
 				webBuilder.UseStartup<Startup>();
@@ -76,21 +83,17 @@ namespace Albatross.Hosting {
 			});
 			return this;
 		}
-
-
 		public virtual Setup ConfigureServiceHost<T>() where T : class, IHostedService {
 			hostBuilder.ConfigureServices((hostContext, services) => {
 				services.AddHostedService<T>();
 			});
 			return this;
 		}
-
 		public virtual void ConfigureServices(IServiceCollection services, IConfiguration configuration) {
 			services.TryAddSingleton(new ProgramSetting(configuration));
 			services.TryAddSingleton(EnvironmentSetting.ASPNETCORE_ENVIRONMENT);
 			services.TryAddSingleton(provider => provider.GetRequiredService<ILoggerFactory>().CreateLogger("default"));
 		}
-
 		public virtual async Task RunAsync() {
 			using var logger = this.setupSerilog.Create();
 			this.hostBuilder.ConfigureServices((context, services) => this.ConfigureServices(services, context.Configuration));
