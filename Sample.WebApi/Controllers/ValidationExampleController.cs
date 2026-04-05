2cacae34df3fbf90e19457d0b26b654359b0fc6e
rushuiguan <rushui@gmail.com>
2026-04-05 15:51:59 -0400

    add validation testing

--- Sample.WebApi/Controllers/ValidationExampleController.cs
+++ Sample.WebApi/Controllers/ValidationExampleController.cs
@@ -2,7 +2,7 @@ using Albatross.Hosting;
 using Albatross.Input;
 using Microsoft.AspNetCore.Mvc;
 using Sample.Core.Requests;
-using System.Threading;
+using System;
 
 namespace Sample.WebApi.Controllers {
 	[Route("api/[controller]")]
@@ -17,5 +17,31 @@ namespace Sample.WebApi.Controllers {
 			// Do something with sanitized request
 			return Ok();
 		}
+
+
+		[HttpGet("{resource}")]
+		public void Get([FromRoute] ResourceName resource) {
+		}
+	}
+
+	public struct ResourceName: IParsable<ResourceName>
+		public string Value { get; }
+		public ResourceName(string value) {
+			this.Value = value;
+		}
+		public static ResourceName Parse(string s, IFormatProvider provider) {
+			if (string.IsNullOrEmpty(s)) {
+				throw new FormatException("Resource name cannot be empty");
+			}
+			return new ResourceName(s);
+		}
+
+		public static bool TryParse(string s, IFormatProvider provider, out ResourceName result) {
+			if (string.IsNullOrEmpty(s)) {
+				result = default;
+				return false;
+			}
+			result = new ResourceName(s);
+			return true; {
 	}
 }
